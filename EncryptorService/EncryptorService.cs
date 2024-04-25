a904da8b32b921317078f7937b99a545e9e43ee6
Patryk Wróblewski <52327738+Nitrej@users.noreply.github.com>
2024-04-25 19:05:41 +0200

    Upload part2

---
+++ EncryptorService/EncryptorService.cs
@@ -0,0 +1,47 @@
+﻿using BibliotekaSzyfrowania;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptorService
+{
+    public partial class EncryptorService : ServiceBase
+    {
+        private ServiceHost serviceHost = null;
+        public EncryptorService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            serviceHost = new ServiceHost(typeof(Szyfrowanie));
+            try
+            {
+                serviceHost.AddServiceEndpoint(typeof(ISzyfrowanie), new WSHttpBinding(), "Encryptor");
+
+                serviceHost.Open();
+            }
+            catch (CommunicationException ce)
+            {
+                serviceHost.Abort();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+                serviceHost.Close();
+        }
+    }
+}
---
+++ EncryptorService/ProjectInstaller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptorService
+{
+    [RunInstaller(true)]
+    public class ProjectInstaller : Installer
+    {
+        public ServiceProcessInstaller process;
+        public ServiceInstaller service;
+
+        public ProjectInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+
+            service.ServiceName = "EncryptorService";
+            service.DisplayName = "Encryptor";
+            service.Description = "Usługa umożliwiająca działanie aplikacji Encryptor.";
+
+            service.StartType = ServiceStartMode.Automatic;
+
+            Installers.Add(process);
+            Installers.Add(service);
+
+            service.AfterInstall += ServiceAfterInstall;
+            service.BeforeInstall += ServiceBeforeUninstall;
+        }
+        private void ServiceAfterInstall(object sender, InstallEventArgs e)
+        {
+            using (ServiceController sc = new ServiceController(service.ServiceName))
+            {
+                sc.Start();
+            }
+        }
+        private void ServiceBeforeUninstall(object sender, InstallEventArgs e)
+        {
+            using (ServiceController sc = new ServiceController(service.ServiceName))
+            {
+                if (sc.Status == ServiceControllerStatus.Running || sc.Status == ServiceControllerStatus.Paused)
+                {
+                    sc.Stop();
+                    sc.WaitForStatus(ServiceControllerStatus.Stopped);
+                }
+            }
+        }
+
+    }
+}
