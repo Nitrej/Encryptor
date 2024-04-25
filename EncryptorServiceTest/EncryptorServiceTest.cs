eef302d8eaffa5ee376d8049b971799ff1a15e2a
Patryk Wróblewski <52327738+Nitrej@users.noreply.github.com>
2024-04-25 19:06:33 +0200

    Upload part 3

---
+++ EncryptorServiceTest/EncryptorServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using EncryptorService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceProcess;
+
+namespace EncryptorServiceTest
+{
+    [TestClass]
+    public class EncryptorServiceTest
+    {
+        [TestMethod]
+        public void EncryptorServiceServiceName()
+        {
+            EncryptorService.EncryptorService encryptorService = new EncryptorService.EncryptorService();
+            Assert.IsTrue(encryptorService.ServiceName == "EncryptorService","Poprawna nazwa usługi");
+        }
+
+        [TestMethod]
+        public void EncryptorServiceNotNull()
+        {
+            EncryptorService.EncryptorService encryptorService = new EncryptorService.EncryptorService();
+
+            Assert.IsNotNull(encryptorService);
+        }
+        [TestMethod]
+        public void EncryptorServiceServiceNameOnBoth()
+        {
+            EncryptorService.EncryptorService encryptorService = new EncryptorService.EncryptorService();
+            EncryptorService.ProjectInstaller projectInstaller = new ProjectInstaller();
+            Assert.AreEqual(encryptorService.ServiceName,projectInstaller.service.ServiceName);
+        }
+    }
+}
---
+++ EncryptorServiceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EncryptorServiceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EncryptorServiceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f37eaf6-88a9-4a03-bbc6-8a6c9bc25a94")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
